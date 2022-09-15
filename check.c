d425f18ec2295c7542aefb57455bdaef15b4fb5f
krishna-csai <krishna2021csai133@abesit.edu.in>
2022-09-16 01:12:28 +0530

    fsdf

--- check.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-int main() {
-    double x = 1000.0;
-    printf("natural log of %.1f is %.4f. \n", x, log(x));
-    printf("log10 of %.1f is %.4f. \n", x, log10(x));
-    return 0;
-}
--- dictionary.py
+++
@@ -1,6 +0,0 @@
-x = input("enter the first number")
-y = input("eneter the second number")
-
-z = int(x) + int(y) 
-
-print(f"{z}")
