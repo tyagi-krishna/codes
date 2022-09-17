2b9b144fcf3a0352909fbb2ca22dc367f7814e09
krishna-csai <krishna2021csai133@abesit.edu.in>
2022-09-18 01:14:39 +0530

    asd

---
+++ html-css-javascript/counter.js
@@ -0,0 +1,12 @@
+let counter = 0 
+function count() {
+    counter++;
+    document.querySelector('h1').innerHTML = counter;
+
+    if(counter%10 ===0) {
+        alert(`counter is ${counter}`);
+    }
+}
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('button').addEventListener('click', count);
+});
